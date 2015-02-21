c90b567c6d0d3d17fc96e022586cccc6b0bc3234
Kaintoras <josgarcia99@gmail.com>
2015-02-21 10:31:21 +0100

    Joseph_2_Commit
    Carpeta Materials
    Codigo Movimiento en x simple

---
+++ Assets/src/Josh_Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Josh_Movement : MonoBehaviour {
+
+	public float speed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 imputDir = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, 0.0f);
+		
+		imputDir *= speed * Time.deltaTime;
+		
+		transform.Translate (imputDir, Space.World);
+		
+		/*if ((transform.position.x + imputDir.x) >= -3.5f && (transform.position.z + imputDir.z) <= 5.0f) { 
+			
+			transform.Translate (imputDir, Space.World);
+			
+		}*/
+
+	}
+}
