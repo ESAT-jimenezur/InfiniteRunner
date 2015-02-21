0119374098be9bb3883ff3e423387b6ae8202e21
ESAT-jimenezur <jimenezur@esat-alumni.com>
2015-02-21 10:20:24 +0100

    Player Movement Added

--- Assets/src/PlayerMovement.cs
+++ Assets/src/PlayerMovement.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log ("ASD");
+
+	private float velocity = 2.0f;
+	private float acceleration = 0.0f;
+
+	private GameObject player, cube1, cube2;
+
+
+	void Start(){
+		//If we need this later
+		player = GameObject.Find ("Player");
+		cube1 = GameObject.Find ("c1");
+		cube2 = GameObject.Find ("c2");
 	}
-	
-	// Update is called once per frame
+
+
 	void Update () {
-	
+		Control ();
 	}
+
+	private void Control(){
+
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(-Vector3.up * (velocity*100) * Time.deltaTime);
+		}else if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.up * (velocity*100) * Time.deltaTime);
+		}
+
+	}
+
+
 }
